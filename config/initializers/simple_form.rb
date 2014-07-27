0d3b33d87b9ebae9e5d66ba62a8561c3733301e7
Edward Loveall <edward@edwardloveall.com>
2014-07-27 16:18:29 -0400

    Refactor invite checkboxes

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -1,9 +1,7 @@
 SimpleForm.setup do |config|
   config.wrappers :default, class: :input, error_class: 'field-with-errors' do |b|
-    b.use :placeholder
-    b.use :label
+    b.use :label_input
     b.optional :hint,  wrap_with: { tag: :span, class: :hint }
-    b.use :input
     b.use :error, wrap_with: { tag: :span, class: :error }
 
     config.error_notification_class = 'alert alert-error'
