1ecd6724b5ff2af3403aa680ccad5b2f81142013
Edward Loveall <edward@edwardloveall.com>
2014-08-10 19:42:43 -0400

    [Fix] regression where choices wouldn't appear selected

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,5 +1,5 @@
 class Choice < ActiveRecord::Base
   belongs_to :person
 
-  enum meal: { steak: 0, lobster: 1, 'chicken_fingers_(kids)' => 2 }
+  enum meal: { steak: 0, lobster_ravioli: 1, 'chicken_fingers_(kids)' => 2 }
 end
